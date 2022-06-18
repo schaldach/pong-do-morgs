3b58d6a10b42694a8e0db38213c1bafabcdcb88c
schaldach <gabimorgado0311@gmail.com>
2022-06-18 17:48:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -463,7 +463,7 @@ function drawParticles(){
                 }
             }
             particlearea['particles'].forEach(part => {
-                ellipse(particlearea.x+part.x+particlearea.frame*12*particlearea.direction*Math.abs(part.x)/20, particlearea.y+part.y+particlearea.frame*5*part.vertical*Math.abs(part.y)/20, 9)
+                ellipse(particlearea.x+part.x+particlearea.frame*12*particlearea.direction*Math.abs(part.x)/20, particlearea.y+part.y+particlearea.frame*4*part.vertical*Math.abs(part.y)/20, 10)
             })
         }
         particlearea.frame++
